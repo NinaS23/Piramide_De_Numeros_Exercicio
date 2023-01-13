7ff43608c2dae05c69942c84e4a1d50677447b1c
MariaDuda <mariabevenuto062@gmail.com>
2023-01-13 15:01:29 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.println(i);
+            }
+            
+        }
+    }
+}
