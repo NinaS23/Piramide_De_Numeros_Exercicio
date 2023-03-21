0fa5849176eb9f06c6b85c151ce6a824f833e08c
DUDA <mariabevenuto062@gmail.com>
2023-03-21 14:29:02 -0300

    fix: error at print i

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        for(int i = 1; i <= 5; i++){
-            for(int j = 1; j <= i; j++){
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.println(i);
             }
-            
         }
     }
 }
